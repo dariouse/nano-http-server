f1e3787ffe3df482d8768c01a3220e27e2ca87ed
ys.kim <ys.kim@okestro.com>
2025-06-12 00:43:21 +0900

    Initial commit

---
+++ src/main/java/com/server/Main.java
@@ -0,0 +1,76 @@
+package com.server;
+
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static final int PORT = 8080;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(10); // 최대 10개 동시 처리
+    private static final boolean nagle = false;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("HTTP 서버 시작됨: http://localhost:" + PORT);
+
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                clientSocket.setTcpNoDelay(nagle);
+
+                executor.submit(() -> {
+                    try {
+                        handleClient(clientSocket);
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void handleClient(Socket clientSocket) throws IOException, InterruptedException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        OutputStream out = clientSocket.getOutputStream();
+
+        String requestLine = in.readLine();
+        System.out.println("요청: " + requestLine);
+        String response;
+
+        if (!nagle) {
+            System.out.println("nagle true");
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            Thread.sleep(100); // 의도적으로 딜레이
+            out.write("Content-Type: text/plain\r\n".getBytes());
+            Thread.sleep(100);
+            out.write("Content-Length: 24\r\n".getBytes());
+            out.write("\r\n".getBytes());
+            out.write("Hello, world! nagle true".getBytes());
+
+            out.flush();
+        } else {
+            System.out.println("nagle false");
+            response = """
+                    HTTP/1.1 200 OK
+                    Content-Type: text/plain
+                    Content-Length: 24
+                    
+                    Hello, world! nagle false
+                    """;
+
+            out.write(response.getBytes());
+            out.flush();
+        }
+    }
+}
